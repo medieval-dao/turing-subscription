a48b0b033e670481a8428f9ec450ffc39f7e8542
0xEstarriol <Estarriol.Vetch@protonmail.com>
2022-04-01 14:33:58 -0400

    add update

---
+++ deploy/01_turing-subscription.js
@@ -0,0 +1,34 @@
+const networkName = hre.network.name
+
+const {ethers} = require('hardhat');
+
+
+module.exports = async ({
+    getNamedAccounts,
+    deployments,
+    getChainId,
+    getUnnamedAccounts,
+  }) => {
+
+    const {deploy} = deployments;
+    let accounts = await ethers.getSigners();
+    let deployer = accounts[0];
+    console.log("=============================");
+    console.log("deployer: ", deployer.address);
+    console.log("=============================");
+
+    let turingCredit;
+    if(networkName == "bobaRinkeby"){
+      turingCredit = "0x208c3CE906cd85362bd29467819d3AcbE5FC1614";
+    }
+  
+    // the following will only deploy "GenericMetaTxProcessor" if the contract was never deployed or if the code changed since last deployment
+    let turingSubscriptionManager = await deploy('TuringSubscriptionManager', {
+      from: deployer.address,
+      gasLimit: 4000000,
+      args: [
+        turingCredit
+      ],
+    });
+    console.log("turingSubscriptionManager deployed: ", turingSubscriptionManager.address);
+  };
